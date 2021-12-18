7d3f45cd64bbffc3a6d9e57efdfc9180c708e90e
Leon Si <contact@leonzalion.com>
2021-12-18 13:12:56 -0500

    update

--- scripts/hooks/commit-msg.ts
+++ scripts/hooks/commit-msg.ts
@@ -1,7 +1,9 @@
-#!/usr/bin/env -S npx ts-node-transpile-only --script-mode
-
 import { execaSync } from 'execa';
 
 const message = process.argv[process.argv.length - 1];
+
+if (message === undefined) {
+	throw new Error('No message provided.');
+}
 
 execaSync('pnpm', ['exec', 'commitlint', '--edit', message]);
--- scripts/hooks/pre-commit.ts
+++ scripts/hooks/pre-commit.ts
@@ -1,17 +1,3 @@
-#!/usr/bin/env -S npx ts-node-transpile-only --script-mode
-
 import { execaCommandSync } from 'execa';
 
-function precommit() {
-	const branch = execaCommandSync(
-		'git rev-parse --symbolic-full-name --abbrev-ref HEAD'
-	).stdout.toString();
-
-	if (branch === 'dev') {
-		execaCommandSync('pnpm exec lint-staged', {
-			stdout: process.stdout,
-		});
-	}
-}
-
-precommit();
+execaCommandSync('pnpm exec lint-staged', { stdio: 'inherit' });
