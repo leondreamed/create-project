6433342d806acf5c6a031eca49d7467b683850bb
Leon Si <contact@leonzalion.com>
2022-01-06 11:29:30 -0500

    dont create lockfiles

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,13 +5,6 @@ import { execaCommandSync } from 'execa';
 const rootPath = new URL('..', import.meta.url).pathname;
 
 fs.rmSync(path.join(rootPath, 'dist'), { recursive: true, force: true });
-execaCommandSync('pnpm i --lockfile-only', {
-	cwd: path.join(rootPath, './src/templates/typescript'),
-});
-execaCommandSync('pnpm i --lockfile-only', {
-	cwd: path.join(rootPath, './src/templates/electron-typescript-vite'),
-});
-
 execaCommandSync('tsc -p tsconfig.build.json');
 execaCommandSync('tsc-alias -p tsconfig.build.json');
 execaCommandSync('cp -r package.json src/templates dist/');
