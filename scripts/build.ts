20b249f6bcc4a889422fcde23cafde63cde467dc
Leon Si <contact@leonzalion.com>
2022-01-12 14:19:07 -0500

    fix: updates

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,10 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { execaCommandSync } from 'execa';
+import { execaCommandSync as exec } from 'execa';
 
 const rootPath = new URL('..', import.meta.url).pathname;
 
 fs.rmSync(path.join(rootPath, 'dist'), { recursive: true, force: true });
-execaCommandSync('tsc -p tsconfig.build.json');
-execaCommandSync('tsc-alias -p tsconfig.build.json');
-execaCommandSync('cp -r package.json src/templates dist/');
+exec('tsc -p tsconfig.build.json');
+exec('tsc-alias -p tsconfig.build.json');
+exec('cp -r package.json src/templates dist/');
