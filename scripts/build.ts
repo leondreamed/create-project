6090d7f0b9269f2be613786d235e022ef62e037a
Leon Si <contact@leonzalion.com>
2022-01-18 19:48:27 -0500

    refactor: use lion system

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,7 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import { execaCommandSync as exec } from 'execa';
+import { rmDist, copyPackageFiles } from 'lion-system';
 
-const rootPath = new URL('..', import.meta.url).pathname;
-
-fs.rmSync(path.join(rootPath, 'dist'), { recursive: true, force: true });
+rmDist();
 exec('tsc -p tsconfig.build.json');
 exec('tsc-alias -p tsconfig.build.json');
-exec('cp -r package.json src/templates dist/');
+await copyPackageFiles(['src/templates']);
