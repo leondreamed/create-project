25544904105b47707840b30261b1a86089437e8d
Leon Si <contact@leonzalion.com>
2022-01-19 12:20:24 -0500

    fix: build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,9 @@
+import * as fs from 'node:fs';
 import { execaCommandSync as exec } from 'execa';
 import { rmDist, copyPackageFiles } from 'lion-system';
 
 rmDist();
 exec('tsc -p tsconfig.build.json');
 exec('tsc-alias -p tsconfig.build.json');
-await copyPackageFiles(['src/templates']);
+await copyPackageFiles();
+fs.cpSync('src/templates', 'dist/templates', { recursive: true });
