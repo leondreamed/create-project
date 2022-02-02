d95af34d300553a8fd971b6b060d4d05f4e682b6
Leon Si <contact@leonzalion.com>
2022-02-01 19:52:59 -0500

    fix: tailwind

--- src/templates/vite-typescript/tailwind.config.cjs
+++ src/templates/vite-typescript/tailwind.config.cjs
@@ -1,10 +1,5 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: { extend: {} },
-	plugins: [],
-	shortcuts: {
-		row: 'flex flex-row',
-		column: 'flex flex-col',
-		center: 'items-center justify-center',
-	},
+	plugins: []
 };
