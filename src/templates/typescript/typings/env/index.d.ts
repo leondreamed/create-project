8fa853cc50040e30b51c61de58ee31bdb55eb731
Leon Si <contact@leonzalion.com>
2022-02-20 09:44:52 -0500

    fix: remove env variables

--- src/templates/typescript/typings/env/index.d.ts
+++ src/templates/typescript/typings/env/index.d.ts
@@ -3,10 +3,7 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			TWILIO_ACCOUNT_SID: string;
-			TWILIO_AUTH_TOKEN: string;
-			PHONE_NUMBER_TO_CALL: string;
-			ORIGIN_PHONE_NUMBER: string;
+			// Your .env variables go here
 		}
 	}
 }
