720854ed55ecbdac3bfd5500aab2bedb4165ce68
Leon Si <contact@leonzalion.com>
2022-02-25 22:13:20 -0500

    fix: add github url

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -108,7 +108,7 @@ export async function createProject(options?: CreateProjectOptions) {
 		replace.sync({
 			files: packageJsonPath,
 			from: ['{{repository}}'],
-			to: [`github:${projectRepository.replace(/"/g, '\\"')}`],
+			to: [projectRepository.replace(/"/g, '\\"')],
 		});
 	}
 
