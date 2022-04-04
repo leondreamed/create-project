06abd61254e099c7e5ed15f319a971d8740924d7
Leon Si <contact@leonzalion.com>
2022-04-04 12:15:53 -0400

    fix: node_modules in bundled package

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,17 @@
 import * as fs from 'node:fs';
 import { execaCommandSync as exec } from 'execa';
 import { rmDist, copyPackageFiles, chProjectDir } from 'lion-system';
+import { globbySync } from 'globby';
 
 chProjectDir(import.meta.url);
 rmDist();
 exec('tsc -p tsconfig.build.json');
 exec('tsc-alias -p tsconfig.build.json');
 copyPackageFiles();
+
+// Verify that node_modules does not exist in templates
+if (globbySync('src/templates/**/node_modules').length > 0) {
+	throw new Error('node_modules folder detected in src/templates');
+}
+
 fs.cpSync('src/templates', 'dist/templates', { recursive: true });
