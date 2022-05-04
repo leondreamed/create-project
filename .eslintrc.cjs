f69d724312e69cb1d27cf8bf7ce14cfdbb3ee962
Leon Si <contact@leonzalion.com>
2022-05-04 17:44:09 -0400

    chore: update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 const createESLintConfig = require('@leonzalion/configs/eslint.cjs');
 
 module.exports = createESLintConfig(__dirname, {
-	root: true,
 	ignorePatterns: ['src/templates/**'],
 });
+
