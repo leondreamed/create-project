ebf787b797689e727c73541974d56d64aa15ed6a
Leon Si <contact@leonzalion.com>
2022-05-17 13:00:38 -0400

    fix: eslint

--- test/suites/scoped-package.test.ts
+++ test/suites/scoped-package.test.ts
@@ -1,25 +1,24 @@
-import * as path from 'node:path';
-import { beforeAll, describe } from 'vitest';
+import * as path from "node:path";
+import { beforeAll, describe } from "vitest";
 
-import { ProjectType } from '~/types/project.js';
-import { checkProject } from '~test/utils/check.js';
-import { tempDir } from '~test/utils/path.js';
-import { createProject, removeMyProject } from '~test/utils/project.js';
+import { ProjectType } from "~/types/project.js";
+import { checkProject } from "~test/utils/check.js";
+import { tempDir } from "~test/utils/path.js";
+import { createProject, removeMyProject } from "~test/utils/project.js";
 
-describe('creates valid TypeScript project', () => {
-	const projectDestDir = path.join(tempDir, 'scoped-package');
+describe("creates valid TypeScript project", () => {
+  const projectDestDir = path.join(tempDir, "scoped-package");
 
-	beforeAll(async () => {
-		removeMyProject(projectDestDir);
-		await createProject({
-			projectDestDir,
-			projectName: '@scoped/package',
-			projectType: ProjectType.typescript,
-		});
-	});
+  beforeAll(async () => {
+    removeMyProject(projectDestDir);
+    await createProject({
+      projectDestDir,
+      projectName: "@scoped/package",
+      projectType: ProjectType.typescript,
+    });
+  });
 
-	checkProject({
-		projectDir: projectDestDir,
-		projectName: '@scoped/package',
-	});
+  checkProject({
+    projectDir: projectDestDir,
+  });
 });
--- test/suites/typescript.test.ts
+++ test/suites/typescript.test.ts
@@ -1,30 +1,29 @@
-import * as path from 'node:path';
-import { beforeAll, describe } from 'vitest';
+import * as path from "node:path";
+import { beforeAll, describe } from "vitest";
 
-import { ProjectType } from '~/types/project.js';
-import { checkProject } from '~test/utils/check.js';
-import { tempDir } from '~test/utils/path.js';
+import { ProjectType } from "~/types/project.js";
+import { checkProject } from "~test/utils/check.js";
+import { tempDir } from "~test/utils/path.js";
 import {
-	createProject,
-	getProjectName,
-	removeMyProject,
-} from '~test/utils/project.js';
+  createProject,
+  getProjectName,
+  removeMyProject,
+} from "~test/utils/project.js";
 
-describe('creates valid TypeScript project', () => {
-	const projectName = getProjectName(ProjectType.typescript);
-	const projectDestDir = path.join(tempDir, 'my-typescript-project');
+describe("creates valid TypeScript project", () => {
+  const projectName = getProjectName(ProjectType.typescript);
+  const projectDestDir = path.join(tempDir, "my-typescript-project");
 
-	beforeAll(async () => {
-		removeMyProject(projectDestDir);
-		await createProject({
-			projectDestDir,
-			projectType: ProjectType.typescript,
-			projectName,
-		});
-	});
+  beforeAll(async () => {
+    removeMyProject(projectDestDir);
+    await createProject({
+      projectDestDir,
+      projectType: ProjectType.typescript,
+      projectName,
+    });
+  });
 
-	checkProject({
-		projectDir: projectDestDir,
-		projectName,
-	});
+  checkProject({
+    projectDir: projectDestDir,
+  });
 });
