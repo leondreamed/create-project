178a26a034e20393c3f0df215caccd64ca4cf60b
Leon Si <leon@leondreamed.com>
2022-12-12 20:17:20 -0500

    fix: build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,18 +1,16 @@
-import * as fs from 'node:fs';
+import * as fs from 'node:fs'
 
-import { execaCommandSync as exec } from 'execa';
-import { globbySync } from 'globby';
-import { chProjectDir, copyPackageFiles, rmDist } from 'lionconfig';
+import { globbySync } from 'globby'
+import { chProjectDir, copyPackageFiles, rmDist, tsc } from 'lionconfig'
 
-chProjectDir(import.meta.url);
-rmDist();
-exec('tsc -p tsconfig.build.json');
-exec('tsc-alias -p tsconfig.build.json');
-await copyPackageFiles({ commonjs: false });
+chProjectDir(import.meta.url)
+rmDist()
+await tsc({ tsConfigPath: 'tsconfig.build.json' })
+await copyPackageFiles({ commonjs: false })
 
 // Verify that node_modules does not exist in templates
 if (globbySync('src/templates/**/node_modules').length > 0) {
-	throw new Error('node_modules folder detected in src/templates');
+	throw new Error('node_modules folder detected in src/templates')
 }
 
-fs.cpSync('src/templates', 'dist/templates', { recursive: true });
+fs.cpSync('src/templates', 'dist/templates', { recursive: true })
